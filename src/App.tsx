a96858f5603f0ff9767301305da0be7d2ac70015
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-08-16 23:01:10 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="text-4xl font-bold underline flex items-center justify-center h-screen">
+      Hello, Tailwind v4 + React + Vite + TypeScript!
+    </h1>
+  );
+}
