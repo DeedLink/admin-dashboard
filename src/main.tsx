98ed66a02fe2c96cdaef719db474c6c3539380cb
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-09-29 18:48:06 +0530

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { WalletProvider } from "./contexts/WalletContext";
+import { ToastProvider } from "./contexts/ToastContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ToastProvider>
+      <WalletProvider>
+        <App />
+      </WalletProvider>
+    </ToastProvider>
   </React.StrictMode>
 );
