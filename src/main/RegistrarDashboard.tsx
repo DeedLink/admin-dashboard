7eedde7a60b2de420e0a5b4e537c750466a52f0d
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-10-07 00:09:30 +0530

    update auth login

--- src/main/RegistrarDashboard.tsx
+++ src/main/RegistrarDashboard.tsx
@@ -7,10 +7,12 @@ import IVSLRequests from "../tabs/IVSLRequests";
 import NotaryRequests from "../tabs/NotaryRequests";
 import { useLoader } from "../contexts/LoaderContext";
 import Logs from "../tabs/Logs";
+import { useLogin } from "../contexts/LoginContext";
 
 const AdminDashboard = () => {
   const [activeTab, setActiveTab] = useState("kyc");
   const { showLoader, hideLoader } = useLoader();
+  const { user } = useLogin();
 
   useEffect(()=>{
     showLoader();
@@ -25,13 +27,21 @@ const AdminDashboard = () => {
       <Sidebar activeTab={activeTab} onTabChange={setActiveTab} />
       <div className="flex-1 flex flex-col">
         <Header />
-        <main className="flex-1 overflow-y-auto p-8 space-y-6">
-          {activeTab === "kyc" && <KYCQueue />}
-          {activeTab === "surveyor" && <SurveyorRequests/>}
-          {activeTab === "ivsl" && <IVSLRequests/>}
-          {activeTab === "notary" && <NotaryRequests/>}
-          {activeTab === "logs" && <Logs/>}
-        </main>
+        {
+          user?.token ? (
+            <main className="flex-1 overflow-y-auto p-8 space-y-6">
+              {activeTab === "kyc" && <KYCQueue />}
+              {activeTab === "surveyor" && <SurveyorRequests/>}
+              {activeTab === "ivsl" && <IVSLRequests/>}
+              {activeTab === "notary" && <NotaryRequests/>}
+              {activeTab === "logs" && <Logs/>}
+            </main>
+          ) : (
+            <div className="w-full h-full flex items-center justify-center text-white">
+              Please Activate The Pass To Continue!
+            </div>
+          )
+        }
       </div>
     </div>
   );
