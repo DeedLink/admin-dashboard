988397669e09d599272752386cbf7c203ac258c2
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-10-28 21:49:28 +0530

    user card updated with charts

--- src/tabs/AnalaticsDashboard.tsx
+++ src/tabs/AnalaticsDashboard.tsx
@@ -3,7 +3,7 @@ import UsersCard from "../components/analaticcards/UsersCard";
 
 const AnalaticsDashboard = () => {
   return(
-    <div className="text-white flex md:flex-row flex-col gap-6 w-full">
+    <div className="text-white flex flex-col 2xl:flex-row gap-6 w-full">
         <div className="w-full"><UsersCard/></div>
         <div className="w-full"><DeedCard/></div>
     </div>
